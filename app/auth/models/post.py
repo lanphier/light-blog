e0d15c5de1973ed00d08d0e8719285966e1432f4
simpleapples <zangzhiya@gmail.com>
2015-06-08 15:51:30 +0800

    添加static文件夹

---
+++ app/auth/models/post.py
@@ -0,0 +1,9 @@
+from app import db
+from datetime import datetime
+
+class Post(db.Model):
+    ___tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text)
+    content = db.Column(db.Text, unique=True, index=True)
+    create_time = db.Column(db.DateTime, index=True, default=datetime.utcnow)
