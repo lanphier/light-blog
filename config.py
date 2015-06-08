697ba7983b32f6ee1d9eb30d348aef05a4c18ce0
Lu Yunliang <yunliang.lu7@gmail.com>
2015-06-08 18:11:10 +0800

    add templates && static

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'kaopuchou'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_RECORD_QUERIES = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:19910108@127.0.0.1:5432/light-blog'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:nebula@127.0.0.1:5432/light-blog'
 
     WTF_CSRF_ENABLED = False
     WTF_CSRF_CHECK_DEFAULT = False
